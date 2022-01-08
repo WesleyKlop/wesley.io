8e202bee8522382e030e0959092695f1c8552177
Wesley Klop <wesley19097@gmail.com>
2022-01-08 23:23:27 +0100

    Upgrade npm dependencies

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,13 +11,7 @@ const mix = require('laravel-mix')
  |
  */
 
-mix.js('resources/js/app.js', 'public/js').sass(
-    'resources/sass/app.scss',
-    'public/css',
-)
+mix
+  .js('resources/js/app.js', 'public/js')
+  .sass('resources/sass/app.scss', 'public/css')
 
-// if (mix.inProduction()) {
-//   mix.options({
-//     purifyCss: true,
-//   })
-// }
